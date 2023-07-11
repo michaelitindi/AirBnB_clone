a4baf525a0a1128905a84f8c3221b8c5c56418a5
jeshi2 <jamesvean19@gmail.com>
2023-07-11 13:50:12 +0300

    jeshi

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ Define the BaseModel class
 
 import uuid
 from datetime import datetime
+from models.ingine.__init__ import storage
 
 class BaseModel:
     """
@@ -24,10 +25,13 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at =datetime.now()
+            #from models import storage
+            storage.new(self)
 
     def save(self):
         """update attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """return dict rep. of the object"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Define FileStorage class
+"""
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """Serialize instances to JSON file and deserialize JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """return dict __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """set in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serialize __objects to JSON file"""
+        json_dict = {}
+        for key, obj in self.__objects.items():
+            json_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(json_dict, file)
+
+    def reload(self):
+        """Deserialize the JSOn file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                json_dict = json.load(file)
+                for key, value in json_dict.items():
+                    cls_name, obj_id = key.split('.')
+                    obj_dict = value
+                    obj_dict['__class__'] = cls_name
+                    obj = eval(cls_name)(**obj_dict)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
