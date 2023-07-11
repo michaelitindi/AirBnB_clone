a5a296c6d0ed624a88f5f06105ba082401c6cd1d
michaelitindi <michaelitindi@gmail.com>
2023-07-11 00:22:18 -0700

    in sync now

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,12 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = self.updated_at =datetime.now()
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at =datetime.now()
 
     def save(self):
         """update attribute updated_at with the current datetime"""
